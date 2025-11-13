d6346db65c513dd4124103b9e097b90efdd926dd
Karabo Sambo <karabosambo@outlook.com>
2025-11-13 13:24:55 +0200

    Amended .env variables to account for production app deploymnets

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api/v1';
+
+export const API_BASE_URL = `${process.env.REACT_APP_SPRING_BACKEND_URL}`;
 
 // Images --------------------------------------
 export const searchImages = async (query) => {
